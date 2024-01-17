0ec944c4af6b1db89b6558530cc98b183e29106b
= <=>
2024-01-17 10:01:18 +0100

    🔧 eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,44 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'xo',
-		'plugin:vue/vue3-essential',
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [
-				'.eslintrc.{js,cjs}',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'vue',
-	],
-	rules: {
-	},
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'xo',
+        'plugin:vue/vue3-essential',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'vue',
+    ],
+    rules: {
+        semi: [
+            0,
+            'always',
+        ],
+        'object-curly-spacing': [
+            0,
+            'always',
+        ],
+        indent: [
+            2,
+            4,
+        ],
+    },
 };
