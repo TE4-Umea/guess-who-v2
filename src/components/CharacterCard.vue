c8f92664979f1822a39c6a7f7dc4bcc60892c335
= <=>
2024-01-17 13:50:37 +0100

    💄 Restore styles

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,10 +3,8 @@ defineProps(['character'])
 </script>
 
 <template>
-    <div v-on:click="character.isHidden == true" v-if="!character.isHidden" class="characterCard">
-        <div class="imgWrapper">
-            <img :src="character.image">
-        </div>
+    <div v-on:click="character.isHidden = !character.isHidden" v-if="!character.isHidden" class="characterCard">
+        <img :src="character.image">
         <p>{{ character.name }}</p>
     </div>
     <div v-else class="characterCard">
@@ -16,3 +14,28 @@ defineProps(['character'])
 <script>
 
 </script>
+
+<style>
+.grid .characterCard {
+    min-width: 20vw;
+    height: 100%;
+
+    border: 4px solid red;
+
+    text-align: center;
+    min-height: 100px;
+}
+
+.grid .characterCard img {
+    width: 20vw;
+    height: 20vw;
+    object-fit: cover;
+}
+
+@media screen and (max-width: 768px) {
+    .grid .characterCard img {
+        width: 40vw;
+        height: 40vw;
+    }
+}
+</style>
--- src/components/QuestionButton.vue
+++ src/components/QuestionButton.vue
@@ -13,3 +13,35 @@ defineProps(['question'])
 <script>
 
 </script>
+
+<style>
+#myUL li a {
+    border: 1px solid #ddd;
+    background-color: #28282b;
+
+    margin-top: -1px;
+    padding: 12px;
+    padding-right: 50px;
+
+    text-decoration: none;
+    font-size: 18px;
+    /*Seth was here*/
+
+    display: block;
+    size: 100%;
+}
+
+#myUL li:hover {
+    background-color: #505050;
+}
+
+a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
+}
+
+a:hover {
+    color: #535bf2;
+}
+</style>
