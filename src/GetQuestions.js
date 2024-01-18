e142b06ddd8363f3d7b3458fd70fa623f6f73528
LokeOberg04 <loke.oberg@elev.ga.ntig.se>
2024-01-18 09:30:07 +0100

    tog bort index baserade frågor. yuh

--- src/GetQuestions.js
+++ src/GetQuestions.js
@@ -2,23 +2,15 @@ export async function getQuestionsFromDatabase() {
     const questions = [
         {
             text: 'Do they have swag?',
-            command: 'characters.map(character => { if (!character.swag) { character.isHidden = true } })',
         },
         {
             text: 'Does their name contain L?',
-            command: 'characters.map(character => { character.isHidden = !character.isHidden })',
         },
         {
             text: 'Is the character male?',
-            command: '',
         },
         {
             text: 'Is the character female?',
-            command: '',
-        },
-        {
-            text: '',
-            command: '',
         },
     ];
 
