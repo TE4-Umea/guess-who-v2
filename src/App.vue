c5faf7fb81363b6a171dd7a221adba5d8397d1ce
= <=>
2024-01-19 09:33:35 +0100

    💫 Flip animation for card

--- src/App.vue
+++ src/App.vue
@@ -23,25 +23,16 @@ import { getQuestionsFromDatabase } from './GetQuestions.js';
 export default {
     data() {
         return {
-            characters: [
-                // Remove contents later
-                { name: 'Leo', text: 'Le', image: 'leooberg.png', swag: false, isHidden: false },
-                { name: 'Loke', text: 'BOOY GOT THAT VIRUS', image: 'lokeoberg.png', swag: false, isHidden: false },
-                { name: 'Hjalmar', text: 'Hj', image: 'vue.svg', swag: false, isHidden: false },
-                { name: 'Alexander', text: 'Al', image: 'alex.jpg', swag: true, isHidden: false },
-            ],
-            questions: [
-                { text: 'har han swag?', command: 'characters.map(character => { if (!character.swag) { character.isHidden = true } })' },
-                { text: 'innehåller hans namn L?', command: 'characters.map(character => { character.isHidden = !character.isHidden })' },
-            ],
+            characters: [],
+            questions: [],
         };
-    },
-    methods: {
-
     },
     async mounted() {
         this.characters = await getCharactersFromDatabase();
         this.questions = await getQuestionsFromDatabase();
+    },
+    methods: {
+
     },
 };
 </script>
--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,11 +3,10 @@ defineProps(['character'])
 </script>
 
 <template>
-    <div v-on:click="character.isHidden = !character.isHidden" v-if="!character.isHidden" class="characterCard">
+    <div :class="[{ backsideAnimation: character.isHidden }]" v-on:click="character.isHidden = !character.isHidden"
+        class="characterCard"> <!-- Get class 'backsideAnimation' if isHidden is true -->
         <img :src="character.image">
         <p>{{ character.name }}</p>
-    </div>
-    <div v-else class="characterCard">
     </div>
 </template>
 
@@ -16,26 +15,47 @@ defineProps(['character'])
 </script>
 
 <style>
-.grid .characterCard {
-    min-width: 20vw;
+.characterCard {
+    width: 12vw;
     height: 100%;
+    min-height: 100px;
 
     border: 4px solid red;
 
     text-align: center;
-    min-height: 100px;
+    line-break: normal;
+
+    perspective: 1000px;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+    transition-timing-function: cubic-bezier(1, 1.03, .54, 1.4);
 }
 
-.grid .characterCard img {
-    width: 20vw;
-    height: 20vw;
+.backsideAnimation {
+    transform: rotateX(180deg);
+}
+
+.backsideAnimation img,
+.backsideAnimation p {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0s 0.3s, opacity 0.8s linear;
+}
+
+.characterCard img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
 }
 
+.characterCard p {
+    position: relative;
+    bottom: 50px;
+}
+
 @media screen and (max-width: 768px) {
-    .grid .characterCard img {
-        width: 40vw;
-        height: 40vw;
+    .characterCard {
+        width: 24vw;
     }
 }
 </style>
